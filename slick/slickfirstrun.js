c3234193d208c7dea18e8728f260877f5567c9f0
Anderzenn <madsdk_789@hotmail.com>
2018-03-30 19:04:03 +0200

    Changes to 404
    - Changed 404 layout.
    - Completely rewrote slickfirstrun.js
    - Fixed autoplay speed for carousel.
    - Changed color for disabled input in Contact Me.

--- slick/slickfirstrun.js
+++ slick/slickfirstrun.js
@@ -1,9 +1,43 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
 	$('.slides').slick({
 		arrows: true,
 		dots: true,
-		autoplay: true,
 		easing: "ease",
-		speed: 700
+		autoplay: true, 
 	});
-});
+	
+	var $s = $(".slides");
+
+	$s.slick({ 
+    
+		autoplay: true, 
+		autoplaySpeed: 1000
+    
+	});
+});*/
+
+function createSlick(){  
+
+	$(".slides").not('.slick-initialized').slick({
+    autoplay: true,
+    dots: true,
+	speed: 600,
+	autoplaySpeed: 7000,
+	dragable: false,
+    responsive: [{ 
+        breakpoint: 500,
+        settings: {
+            dots: false,
+            arrows: false,
+            infinite: false,
+			dragable: true
+        } 
+    }]
+});	
+
+}
+
+createSlick();
+
+//Now it will not throw error, even if called multiple times.
+$(window).on( 'resize', createSlick );
